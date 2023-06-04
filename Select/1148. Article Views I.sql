2a2bee5f186b52b7ca42970337a36bfdbddff2bb
anant0059 <2019uce0059@gmail.com>
2023-06-04 15:35:22 +0530

    Easy

--- Select/1148. Article Views I.sql
+++ Select/1148. Article Views I.sql
@@ -52,3 +52,9 @@
 
 -- Solution:
 
+# Write your MySQL query statement below
+
+SELECT DISTINCT(author_id) AS id
+    FROM Views WHERE
+        author_id = viewer_id
+    ORDER BY id ASC
--- Select/1683. Invalid Tweets.sql
+++ Select/1683. Invalid Tweets.sql
@@ -48,7 +48,6 @@
 
 # Write your MySQL query statement below
 
-SELECT DISTINCT(author_id) AS id
-    FROM Views WHERE
-        author_id = viewer_id
-    ORDER BY id ASC
+SELECT tweet_id 
+    FROM Tweets WHERE
+        LENGTH(content) > 15
