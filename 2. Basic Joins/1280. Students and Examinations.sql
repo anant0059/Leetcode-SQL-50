2205135f0a1ef2ff1caf21e012865334bc5334b0
anant0059 <2019uce0059@gmail.com>
2023-06-05 12:31:55 +0530

    Easy

--- 2. Basic Joins/1280. Students and Examinations.sql
+++ 2. Basic Joins/1280. Students and Examinations.sql
@@ -110,3 +110,14 @@
 
 -- Solution:
 
+# Write your MySQL query statement below
+
+SELECT Students.student_id, Students.student_name, Subjects.subject_name, COUNT(Examinations.subject_name) AS attended_exams FROM 
+    (Students JOIN Subjects)
+    LEFT JOIN
+    (Examinations) ON
+        (Examinations.student_id = Students.student_id
+        AND
+        Examinations.subject_name = Subjects.subject_name)
+group by Students.student_id, Subjects.subject_name
+order by Students.student_id
