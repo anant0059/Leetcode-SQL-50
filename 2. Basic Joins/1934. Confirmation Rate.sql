8cac4115a2aadaecb3dca68309cfdeff3c25b84c
anant0059 <2019uce0059@gmail.com>
2023-06-05 13:33:19 +0530

    Medium

--- 2. Basic Joins/1934. Confirmation Rate.sql
+++ 2. Basic Joins/1934. Confirmation Rate.sql
@@ -82,3 +82,12 @@
 
 -- Solution:
 
+# Write your MySQL query statement below
+
+select
+    s.user_id,
+    ifnull(round(sum(action='confirmed')/sum(action='confirmed' or action='timeout'), 2), 0) as confirmation_rate
+from signups s
+left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id
