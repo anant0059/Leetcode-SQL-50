4e3b61997f109130ce7baaa34ce25e3fb98a8256
anant0059 <2019uce0059@gmail.com>
2023-06-06 11:03:28 +0530

    EASY

--- 3. Basic Aggregate Functions/1075. Project Employees I.sql
+++ 3. Basic Aggregate Functions/1075. Project Employees I.sql
@@ -70,3 +70,12 @@
 
 -- Solution:
 
+# Write your MySQL query statement below
+SELECT p.project_id, ROUND(SUM(e.experience_years) / COUNT(p.project_id), 2) AS average_years FROM
+(
+    Project p INNER JOIN Employee e ON
+    (
+        p.employee_id = e.employee_id
+    )
+)
+GROUP BY p.project_id
