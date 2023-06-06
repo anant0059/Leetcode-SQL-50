3e3ce3d1701f09ad79e2965189553f6f8adf9fc7
anant0059 <2019uce0059@gmail.com>
2023-06-06 11:23:14 +0530

    EASY

--- 3. Basic Aggregate Functions/1633. Percentage of Users Attended a Contest.sql
+++ 3. Basic Aggregate Functions/1633. Percentage of Users Attended a Contest.sql
@@ -79,5 +79,16 @@
 
 
 
--- Soluton:
+-- Solution:
 
+# Write your MySQL query statement below
+
+SELECT r.contest_id, ROUND((COUNT(r.contest_id) / (SELECT COUNT(*) FROM users)) * 100, 2) AS percentage FROM
+(
+    Register r INNER JOIN Users u ON
+    (
+        r.user_id = u.user_id
+    )
+)
+GROUP BY r.contest_id
+ORDER BY percentage desc, contest_id asc
