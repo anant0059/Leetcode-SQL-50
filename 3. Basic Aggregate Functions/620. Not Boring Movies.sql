a3421b02e1862f25e922eced3ed4cd093bef5c4c
anant0059 <2019uce0059@gmail.com>
2023-06-06 10:31:42 +0530

    Easy

--- 3. Basic Aggregate Functions/620. Not Boring Movies.sql
+++ 3. Basic Aggregate Functions/620. Not Boring Movies.sql
@@ -50,3 +50,13 @@
 
 -- Solution:
 
+# Write your MySQL query statement below
+
+SELECT * FROM
+    CINEMA WHERE
+        (
+            id % 2 = 1
+            AND
+            description != 'boring'
+        )
+    ORDER BY rating desc
