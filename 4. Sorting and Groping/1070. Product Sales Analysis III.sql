7a0bb9a2cc48047cf9fe618a8cfb321bc66ae487
anant0059 <2019uce0059@gmail.com>
2023-06-07 09:35:35 +0530

    Medium

--- 4. Sorting and Groping/1070. Product Sales Analysis III.sql
+++ 4. Sorting and Groping/1070. Product Sales Analysis III.sql
@@ -69,3 +69,13 @@
 
 -- Solution:
 
+# Write your MySQL query statement below
+
+SELECT
+    product_id,
+    MIN(year) AS first_year,
+    quantity,
+    price
+FROM
+    Sales
+GROUP BY product_id
